e5a5ca062b2c2b60d0cca2429c0b6ccb343a3fce
SasangaME <madhumaledirisinghe@gmail.com>
2022-10-27 11:40:02 +0530

    sender app initialization

---
+++ sender/src/app.ts
@@ -0,0 +1,41 @@
+import express, { Application, Request, Response, NextFunction, Router } from 'express';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
+import { config } from 'dotenv';
+import upload from 'express-fileupload';
+
+config();
+
+const app: Application = express();
+
+app.use(cors({ origin: '*' }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const fileSize: number = parseInt(process.env.FILE_SIZE || '2'); // in MB
+app.use(upload({
+    limits: { fileSize: fileSize * 1024 * 1024 }
+}));
+
+export const router = Router();
+// router.use('/api/v1/build', buildRouter);
+app.use(router);
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.json({ "message": "Express server with TypeScript" });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(statusCode).json({ 'message': err.message });
+    return;
+});
+
+const port = parseInt(process.env.PORT || '3000');
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+})
+

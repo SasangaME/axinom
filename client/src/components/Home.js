c54719016be158895ae9bf3e65b24f0764ae743f
SasangaME <madhumaledirisinghe@gmail.com>
2022-10-28 18:56:35 +0530

    data grid

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,30 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { getData } from '../services/data.service';
 
-const rows = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
-];
+// const rows = [
+//     { id: 1, col1: 'Hello', col2: 'World' },
+//     { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+//     { id: 3, col1: 'MUI', col2: 'is Amazing' },
+// ];
 
 const columns = [
-    { field: 'col1', headerName: 'Column 1', width: 150 },
-    { field: 'col2', headerName: 'Column 2', width: 150 },
+    { field: '_id', headerName: 'ID', width: 200 },
+    { field: 'createdBy', headerName: 'Created By', width: 100 },
+    { field: 'createdDate', headerName: 'Created Date', width: 150 },
+    { field: 'fileName', headerName: 'File Name', width: 150 },
+    { field: 'path', headerName: 'Path', width: 400 },
 ];
 
 function Home() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            setData(await getData());
+        })();
+    }, []);
+
     const handleEvent = (
         params, // GridRowParams
         event, // MuiEvent<React.MouseEvent<HTMLElement>>
         details, // GridCallbackDetails
     ) => {
-        alert(`Movie "${params.row.col1}" clicked`);
+        alert(`${params.row._id}`);
     };
 
     return (
-        <Box sx={{ height: 400, width: '100%' }}>
-            <DataGrid rows={rows} onRowClick={handleEvent} columns={columns} />
+        <Box sx={{ height: 700, width: '100%' }}>
+            <DataGrid
+                rows={data}
+                onRowClick={handleEvent}
+                columns={columns}
+                getRowId={(row) => row._id} />
         </Box>
     )
 }
--- client/src/services/data.service.js
+++ client/src/services/data.service.js
@@ -1,3 +1,6 @@
-export function getData() {
+import axios from 'axios';
 
+export async function getData() {
+    const { data } = await axios.get('http://localhost:4002/api/v1/content');
+    return data;
 }
